c2401a853f043b6cb4527835d0d50296456c3874
Alexander Mendelevych <sashmen5@gmail.com>
2019-12-21 19:36:23 +0200

    first commit with picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import img from '../images/jus63_1.jpeg';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
+    <SEO title="Home"/>
+    <h1>Hi Plehanova Valeriia!!!!!</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <img src={img} alt=""/>
+      {/*<Image/>*/}
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
